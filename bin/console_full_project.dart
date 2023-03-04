f12a92c4c06c0de25cc037ea7129baa080050255
Abhishek12345679 <sah755146@gmail.com>
2023-03-04 18:29:00 +0530

    first commit

---
+++ bin/console_full_project.dart
@@ -0,0 +1,11 @@
+import 'package:console_full_project/console_full_project.dart'
+    as console_full_project;
+
+void main(List<String> arguments) {
+  try {
+    print(
+        'Hello world: ${console_full_project.calculate()}! \n Args sum: ${arguments.fold(0, (previousValue, element) => previousValue + int.parse(element))}');
+  } catch (err) {
+    print(err);
+  }
+}
